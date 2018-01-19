afdb8dd7222341eb3b86907d6765cea0c795bad7
Cecily <cecily.downs@gmail.com>
2018-01-19 14:20:08 -0800

    Finish setup and adjust app text and styles

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>Writing Tracker</Text>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 25,
+  }
+});
